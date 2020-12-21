39a79bff941f7e50382901781b78d25af5e1a7f1
Varit Seekhao <varit.seekhao@gmail.com>
2020-12-21 18:15:43 -0500

    update js file

--- script.js
+++ script.js
@@ -13,8 +13,37 @@ async function getChampsData(champsName){
     const champsUrl = `http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/champion/${champsName}.json`;
     try {
         const response = await axios.get(champsUrl);
-        console.log(response);
+        // console.log(response);
     } catch(error){
         console.log(error);
     }
 }
+
+async function getAllChamps(){
+    const champsUrl = `http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/champion.json`;
+    try {
+        const response = await axios.get(champsUrl);
+        // console.log(response.data.data);
+        // let allChamps = response.data.data;
+        // console.log(renderChamps);
+         renderChamps(response.data.data);
+    } catch(error){
+        console.log(error);
+    }
+}
+getAllChamps();
+
+function renderChamps(allChamps){
+    // let arr = allChamps;
+    // let arr =[2,3,4,5];
+    let list = document.querySelector(".champions-container");
+    // console.log(list);
+    allChamps.forEach((allChamps) => {
+        let champsDiv = document.createElement("div");
+        champsDiv.innerHTML = `
+        <h1>${allChamps}</h1>`
+        list.appendChild(champsDiv);
+    })
+}
+
+renderChamps();
